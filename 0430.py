520f7116ca9699e9db23fdaa0ddcabb2318ac898
PARK SANGMIN <parksangmin_2020@icloud.com>
2021-04-30 20:01:43 +0900

    TEST

--- 0430.py
+++ 0430.py
@@ -1,1 +1,5 @@
-print('Hello world')ss
+stock_start = {'samsugng': 300000, 'naver': 250000, 'coway':3000}
+for company_name in stock_start.keys():
+    print(company_name)
+for company_stock in stock_start.values(): 
+    print(company_stock)
